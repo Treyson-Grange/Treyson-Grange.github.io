6eab3fd3f725e00cae232ec68a4ee584ab9b1596
Treyson Grange <treyson.grange@outlook.com>
2024-01-03 20:08:01 -0700

    cleaned.

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
-/** 
+/**
  * Function isScrolledToBottom
  * Input: None
- * Output: Boolean 
-*/
+ * Output: Boolean
+ */
 function isScrolledToBottom() {
-    console.log(window.innerHeight + window.scrollY >= document.body.offsetHeight);
-    return window.innerHeight + window.scrollY >= document.body.offsetHeight;
+  console.log(
+    window.innerHeight + window.scrollY >= document.body.offsetHeight
+  );
+  return window.innerHeight + window.scrollY >= document.body.offsetHeight;
 }
 
 //Set up event listener for the footer.
-window.addEventListener('scroll', function () {
-    if (isScrolledToBottom()) {
-        document.getElementById('footer').style.display = 'block';
-    } else {
-        document.getElementById('footer').style.display = 'none';
-    }
+window.addEventListener("scroll", function () {
+  if (isScrolledToBottom()) {
+    document.getElementById("footer").style.display = "block";
+  } else {
+    document.getElementById("footer").style.display = "none";
+  }
 });
