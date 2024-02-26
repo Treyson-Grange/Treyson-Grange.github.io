fe801726ff8c809030ecc25600b00916c1c23c44
Treyson Grange <treyson.grange@outlook.com>
2024-02-25 19:35:30 -0700

    :fire: fixed

--- darkmode.js
+++ darkmode.js
@@ -1,31 +1,27 @@
 const darkModeToggle = document.getElementById("dark-mode-toggle");
 const body = document.body;
+const modeIcon = document.getElementById("mode-icon");
 
-// Check if dark mode preference is stored in localStorage
 const isDarkMode = localStorage.getItem("darkModeEnabled") === "true";
 
-// Function to set dark mode
 function setDarkMode(enabled) {
   if (enabled) {
     body.classList.add("dark-mode");
     darkModeToggle.classList.add("light-mode-toggle");
+    modeIcon.classList.remove("fa-moon");
+    modeIcon.classList.add("fa-sun");
   } else {
     body.classList.remove("dark-mode");
     darkModeToggle.classList.remove("light-mode-toggle");
+    modeIcon.classList.remove("fa-sun");
+    modeIcon.classList.add("fa-moon");
   }
 }
 
-// Set dark mode based on stored preference
 setDarkMode(isDarkMode);
 
-// Event listener for toggling dark mode
 darkModeToggle.addEventListener("click", () => {
   const darkModeEnabled = body.classList.toggle("dark-mode");
-
-  // Toggle icons
-  darkModeToggle.classList.toggle("dark-mode-toggle");
-  darkModeToggle.classList.toggle("light-mode-toggle");
-
-  // Store the user's preference in localStorage
+  setDarkMode(darkModeEnabled);
   localStorage.setItem("darkModeEnabled", darkModeEnabled);
 });
